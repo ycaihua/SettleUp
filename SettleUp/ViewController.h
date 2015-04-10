cff327fb6ced6c813fdf43b54d999a014e2d19ee
nevillco <nevillco@bc.edu>
2015-04-10 13:26:47 -0400

    Initial Commit

---
+++ SettleUp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SettleUp
+//
+//  Created by Connor Neville on 4/10/15.
+//  Copyright (c) 2015 connorneville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SettleUp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SettleUp
+//
+//  Created by Connor Neville on 4/10/15.
+//  Copyright (c) 2015 connorneville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
